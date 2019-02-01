9f4d8ecf9ea9ab839729f0a7fe586274f57c22ca
Matheus de Holanda Pereira Ribeiro <35711103822@info.local>
2019-02-01 17:27:04 -0200

    Início do projeto BugMusic

---
+++ BugMusic.sql
@@ -0,0 +1,31 @@
+INSERT INTO generos
+VALUES ('Rock'),('Sertanejo'),('Pop')
+
+INSERT INTO bandas(NOME,ID_GENERO)
+VALUES  ('Ultraje a Rigor',1),
+		('Fernando e Sorocaba',2),
+		('Barão Vermelho',3)
+
+INSERT INTO albuns(NOME,ID_BANDA)
+VALUES  ('Recomeço',1),
+		('Vendaval',2),
+		('Maior Abandonado',3)
+
+INSERT INTO musicas(NOME,ID_ALBUM)
+VALUES  ('Inútil',1),
+		('Mim Quer',2),
+		('Paga Pau',2),
+		('Baby Suporte',3),
+		('Sem Querer Tocar',1),
+		('Vendavagonha',3)
+
+INSERT INTO cantores
+VALUES  ('Roger Moreira')
+		,('Mingau')
+		,('Bacalhau')
+		,('Marcos Kleine')
+		,('Fernando Zorzanello Bonifácio')
+		,('Fernando Fakri de Assis')
+		,('Guto Goffi')
+		,('Maurício Barros')
+		,('Fernando Magalhães')
